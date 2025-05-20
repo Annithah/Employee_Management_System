adf3a0dc7eb0b62ead35ffccb2a92e982fe607ef
Annithah <uwihirweannitha@gmail.com>
2025-05-20 12:57:04 -0700

    insert susscessfully

--- insert.php
+++ insert.php
@@ -3,21 +3,22 @@
 include 'conn.php';
 
 if($_SERVER["REQUEST_METHOD"]=="POST"){
-    $name=$_POST['name'];
-    $email=$_POST['email'];
-    $password=$_POST['password'];
-    $position=$_POST['position'];
-    $salary=$_POST['salary'];
+    $names=$_POST['names'] ?? '';
+    $email=$_POST['email']?? '';
+    $password=$_POST['password']?? '';
+    $position=$_POST['position']?? '';
+    $salary=$_POST['salary']?? '';
 
-    $sql = "INSERT INTO employe_management('name','email','password','position','salary') VALUES($name,$email,$password,$position,$salary)";
-  if ($conn->query($sql)===TRUE) {
-    echo "student inserted successfully!";
-  }else{
-    echo "Error".$conn->error;
-  }
+    $sql = "INSERT INTO employee(names,email,password,position,salary) VALUES('$names','$email','$password','$position','$salary')";
 
-  $conn->close();
-
+  
+    if ($conn->query($sql)=== true) {
+      echo "connection occured successfully";
+    }
+    else {
+        echo "error occured";
+    }
+       $conn->close();
 }
 
 ?>
